0fc18b6580c1a8bebb5716fbe181f18a415f1750
Evan Hahn <me@evanhahn.com>
2013-12-03 13:22:42 -0500

    styling, descriptions, more data, a bunch

--- www/index.php
+++ www/index.php
@@ -1,5 +1,6 @@
 <?php
 
+// security headers
 $csp = "default-src none; style-src 'self'";
 header('Content-Security-Policy: ' . $csp);
 header('X-Content-Security-Policy: ' . $csp);
@@ -8,16 +9,17 @@ header('X-Frame-Options: DENY');
 header('X-XSS-Protection: 1; mode=block');
 header_remove('X-Powered-By');
 
+// other headers
+header('Content-Type: text/html; charset=utf-8');
+
 ?>
 
 <!DOCTYPE html>
-
-<!-- lovingly made by Evan Hahn. -->
-
 <html lang="en">
 <head>
 
 <meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link rel="stylesheet" href="the.css">
 <title>two-factor authentication list</title>
 
@@ -25,23 +27,50 @@ header_remove('X-Powered-By');
 <body>
 
 <header>
-	<div class="container">
-		<h1>two-factor authentication list</h1>
+
+	<div class="logo">
+		<div class="container">
+			<h1>two-factor authentication list</h1>
+		</div>
 	</div>
+
+	<div class="authors">
+		<div class="container">
+			<p>lovingly made by <a href="http://evanhahn.com">Evan Hahn</a>. ❤</p>
+		</div>
+	</div>
+
+	<div class="description">
+		<div class="container">
+			<p>Two-factor authentication requires an additional step in addition to entering your password, usually by sending a message to your mobile phone.</p>
+		</div>
+	</div>
+
 </header>
 
+<?php $services = json_decode(file_get_contents('./data.json'))->services ?>
+
 <ul id="service-list">
+	<div class="container">
 
-	<?php $services = json_decode(file_get_contents('./data.json'))->services ?>
+		<?php foreach ($services as &$service): ?>
+			<li>
+				<h1><?php echo $service->name ?></h1>
+				<?php if (isset($service->url)): ?>
+					<a title="Enable it" href="<?php echo $service->url ?>">
+						Enable it
+					</a>
+				<?php endif ?>
+				<?php if (isset($service->howto)): ?>
+					<a title="How-to" href="<?php echo $service->howto ?>">
+						How-to
+					</a>
+				<?php endif ?>
+			</li>
+			</a>
+		<?php endforeach ?>
 
-	<?php foreach ($services as &$service): ?>
-
-		<a href="<?php echo $service->url ?>" title="<?php echo $service->name ?>">
-			<li><?php echo $service->name ?></li>
-		</a>
-
-	<?php endforeach ?>
-
+	</div>
 </ul>
 
 </body>
