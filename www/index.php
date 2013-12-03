114106b85699313e3581caeb83e3415c5c3e4976
Evan Hahn <me@evanhahn.com>
2013-12-03 12:30:43 -0500

    skeleton website

---
+++ www/index.php
@@ -0,0 +1,42 @@
+<?php
+
+$csp = "default-src none; style-src 'self'";
+header('Content-Security-Policy: ' . $csp);
+header('X-Content-Security-Policy: ' . $csp);
+header('X-WebKit-CSP: ' . $csp);
+header('X-Frame-Options: DENY');
+header('X-XSS-Protection: 1; mode=block');
+header_remove('X-Powered-By');
+
+?>
+
+<!DOCTYPE html>
+
+<!-- lovingly made by Evan Hahn. -->
+
+<html lang="en">
+<head>
+
+<meta charset="utf-8">
+<link rel="stylesheet" href="the.css">
+<title>two-factor authentication list</title>
+
+</head>
+<body>
+
+<ul id="service-list">
+
+	<?php $services = json_decode(file_get_contents('./data.json'))->services ?>
+
+	<?php foreach ($services as &$service): ?>
+
+		<a href="<?php echo $service->url ?>" title="<?php echo $service->name ?>">
+			<li><?php echo $service->name ?></li>
+		</a>
+
+	<?php endforeach ?>
+
+</ul>
+
+</body>
+</html>
