398dc96b40b56e50a6166d7533149ca55f78c376
Evan Hahn <me@evanhahn.com>
2013-12-11 10:47:25 -0500

    minor: break up credits into not-one-massive-line

--- index.php
+++ index.php
@@ -37,7 +37,16 @@ header('X-UA-Compatible: IE=edge');
 	<div class="authors">
 		<div class="container">
 
-			<p>lovingly made by <a href="http://evanhahn.com">Evan Hahn</a> with contributions from <a href="http://mathiasjakobsen.dk/">Mathias Jakobsen</a>, <a href="http://williamella.com/">Billy Ella</a>, Andrew Uhl, and <a href="http://twitter.com/_MattDolan">Matt Dolan</a>. ❤</p>
+			<p>
+
+				lovingly made by <a href="http://evanhahn.com">Evan Hahn</a> with contributions from
+
+				<a href="http://mathiasjakobsen.dk/">Mathias Jakobsen</a>,
+				<a href="http://williamella.com/">Billy Ella</a>,
+				Andrew Uhl,
+				and <a href="http://twitter.com/_MattDolan">Matt Dolan</a>.
+
+			❤</p>
 
 		</div>
 	</div>
